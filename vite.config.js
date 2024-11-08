c55456191a82b8a03b3a54f9e2d066e836ce2568
Parikshit <parikshit.s.joshi@gmail.com>
2024-11-09 02:03:08 +0530

    deploying

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/yugen-website-react/', // Add this line (replace 'your-repo-name' with your actual repository name)
 })
+
+
+
+
+
+
