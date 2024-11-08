68d2db332424473f09d88a716a9cd9b6259ff3f9
Parikshit <parikshit.s.joshi@gmail.com>
2024-11-09 02:30:19 +0530

    custom domain changes

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/yugen-website-react/', // Add this line (replace 'your-repo-name' with your actual repository name)
+  base: '/', // Add this line (replace 'your-repo-name' with your actual repository name)
 })
 
 
